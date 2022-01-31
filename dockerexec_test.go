170a5235fe4b182e0943d98f3579e792d868fc7e
Segev Finer <segev208@gmail.com>
2022-01-31 22:25:38 +0200

    Add more tests

--- dockerexec_test.go
+++ dockerexec_test.go
@@ -2,7 +2,9 @@ package dockerexec_test
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/docker/docker/api/types"
@@ -10,6 +12,7 @@ import (
 	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/segevfiner/dockerexec"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const testImage = "ubuntu:focal"
@@ -24,13 +27,19 @@ func TestMain(m *testing.M) {
 		panic(err)
 	}
 
-	pullOutput, err := dockerClient.ImagePull(context.Background(), testImage, types.ImagePullOptions{})
-	if err != nil {
-		panic(err)
-	}
-	defer pullOutput.Close()
+	if _, _, err := dockerClient.ImageInspectWithRaw(context.Background(), testImage); err != nil {
+		if client.IsErrNotFound(err) {
+			pullOutput, err := dockerClient.ImagePull(context.Background(), testImage, types.ImagePullOptions{})
+			if err != nil {
+				panic(err)
+			}
+			defer pullOutput.Close()
 
-	jsonmessage.DisplayJSONMessagesStream(pullOutput, os.Stderr, 0, false, nil)
+			jsonmessage.DisplayJSONMessagesStream(pullOutput, os.Stderr, 0, false, nil)
+		} else {
+			panic(err)
+		}
+	}
 
 	os.Exit(m.Run())
 }
@@ -39,7 +48,40 @@ func TestEcho(t *testing.T) {
 	cmd := dockerexec.Command(dockerClient, testImage, "sh", "-c", "echo Hello, World!")
 
 	output, err := cmd.Output()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	assert.Equal(t, "Hello, World!\n", string(output))
+}
+
+func TestCatStdin(t *testing.T) {
+	const input = "Line 1\nLine 2"
+	cmd := dockerexec.Command(dockerClient, testImage, "cat")
+	cmd.Stdin = strings.NewReader(input)
+
+	output, err := cmd.Output()
+	require.NoError(t, err)
+
+	assert.Equal(t, input, string(output))
+}
+
+func TestCatFileRace(t *testing.T) {
+	cmd := dockerexec.Command(dockerClient, testImage, "cat")
+
+	stdin, err := cmd.StdinPipe()
+	require.NoError(t, err)
+
+	err = cmd.Start()
+	require.NoError(t, err)
+
+	wrote := make(chan bool)
+	go func() {
+		defer close(wrote)
+		fmt.Fprint(stdin, "echo\n")
+		stdin.Close()
+	}()
+
+	err = cmd.Wait()
+	require.NoError(t, err)
+
+	<-wrote
 }
